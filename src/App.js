325736dc5ed88ad7f2bb1016193ab866cf660432
Igor Novak <bezengi@gmail.com>
2018-07-15 02:09:15 +0200

    Add bootstrap, add data to post card

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import PostCard from './components/PostCard';
 import comments from './data';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {comments.map((_, i) => <PostCard key={i} data={_} />)}
+        <ul className="list-unstyled">
+          {comments.map((_, i) => <PostCard key={i} data={_} />)}
+        </ul>
       </div>
     );
   }
--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -3,13 +3,22 @@ import React, { Component } from 'react';
 class PostCard extends Component {
 
   render() {
+
     return (
-      <div>
-        <strong>{this.props.data.author}</strong>
-        <p>
-          {this.props.data.content}
-        </p>
+
+      <div className="card">
+        <div className="card-body">
+          <li className="media">
+            <img className="mr-3" src={this.props.data.avatar} alt={this.props.data.author} />
+            <div className="media-body">
+              <h5 className="mt-0">{this.props.data.author}</h5>
+              <small className="text-muted">{this.props.data.date}</small>
+              <p>{this.props.data.content}</p>
+            </div>
+          </li>
+        </div>
       </div>
+
     )
   }
 
